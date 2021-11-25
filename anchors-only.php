42afe2854fcd33e8155c14bd2d497c7f3ccdeeac
Bego Mario Garde <pixolin@pixolin.de>
2021-11-25 16:09:29 +0100

    Initial Commit after defining shortcode

---
+++ anchors-only.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name:     Anchors Only
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Ersetze <em>Scroll to Anchor</em> und zeige nur die Anker.
+ * Author:          Bego Mario Garde
+ * Author URI:      https://pixolin.de
+ * Version:         0.1.0
+ *
+ * @package         Anchors_Only
+ */
+
+// Don't allow direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Run this plugin only if plugin Scroll-to-Anchor has been deactivated
+ *
+ * @return void
+ */
+function check_some_other_plugin() {
+	if ( ! is_plugin_active( 'scroll-to-anchor/scroll-to-anchor.php' ) ) {
+
+	}
+}
+add_action( 'admin_init', 'check_some_other_plugin' );
+
+
+define( 'AO_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+require_once AO_PLUGIN_PATH . 'class-shortcode.php';
+
+if ( is_admin() ) {
+	require_once AO_PLUGIN_PATH . 'class-editor.php';
+}
+
+$anchors_only = new Shortcode();
